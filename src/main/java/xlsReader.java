ad285be824f5bbe084c8093747e54f4ec7275623
Davide Nastri <d.nastri@gmail.com>
2019-05-27 21:29:58 +0200

    feat: 1st commit

---
+++ src/main/java/xlsReader.java
@@ -0,0 +1,54 @@
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class xlsReader {
+
+    public static void main(String[] args) {
+
+
+        try {
+
+            File myFile = new File("e://test.xlsx");
+            FileInputStream fis = new FileInputStream(myFile);
+            XSSFWorkbook myWorkBook = new XSSFWorkbook(fis);
+            XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+            Iterator<Row> rowIterator = mySheet.iterator();
+
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case Cell.CELL_TYPE_BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t");
+                            break;
+                        default:
+                    }
+                }
+                System.out.println("");
+            }
+
+        } catch (IOException e) {
+            System.err.println("An IOException was caught :" + e.getMessage());
+        }
+
+    }
+
+
+
+}
